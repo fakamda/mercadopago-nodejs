339622e1cc405d4f9f8665536b1f9dc5e269409d
fakamda <facundoelcardenal@gmail.com>
2023-12-23 21:30:56 -0300

    routes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-
-import express from 'express';
+import express from 'express'
+import PaymentRoutes from './router/payment.routes.js'
 
 const app = express();
 const port = process.env.PORT || 3000;
